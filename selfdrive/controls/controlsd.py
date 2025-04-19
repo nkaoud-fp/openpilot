bcfc3c5b50748f545ecf9841c9bdf535ce58aae3
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-04-19 09:29:49 +0300

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -580,24 +580,27 @@ class Controls:
       rlx_personality = log.LongitudinalPersonality.relaxed
       speed_kph = CS.vEgo * 3.6
       crnt_personality = self.params.get("LongitudinalPersonality", encoding="utf8")
+      TrafficModeStatus = self.params.get('ChangTrafficModeStat')
       
       if crnt_personality is None:
         crnt_personality = str(stn_personality)
-      if speed_kph < 3 : #and not self.params.get('ChangTrafficModeStat'):  # turn on at 11 kph
+      if speed_kph < 3 and not TrafficModeStatus:  # turn on at 11 kph
         params.put_bool("ChangTrafficModeStat", True)
         params.put_bool("ChangTrafficModeReq", True)
-        #params.put_bool("ChangTrafficModeStat", False)
       elif 25 < speed_kph < 60 and crnt_personality != str(agr_personality):
-        params.put_bool("ChangTrafficModeStat", False)
-        params.put_bool("ChangTrafficModeReq", True)
+        if TrafficModeStatus :
+          params.put_bool("ChangTrafficModeStat", False)
+          params.put_bool("ChangTrafficModeReq", True)
         self.params.put_nonblocking('LongitudinalPersonality', str(agr_personality))
       elif 65 < speed_kph < 110 and crnt_personality != str(stn_personality):
-        params.put_bool("ChangTrafficModeStat", False)
-        params.put_bool("ChangTrafficModeReq", True)
+        if TrafficModeStatus :
+          params.put_bool("ChangTrafficModeStat", False)
+          params.put_bool("ChangTrafficModeReq", True)
         self.params.put_nonblocking('LongitudinalPersonality', str(stn_personality))
       elif speed_kph > 115 and crnt_personality != str(rlx_personality):
-        params.put_bool("ChangTrafficModeStat", False)
-        params.put_bool("ChangTrafficModeReq", True)
+        if TrafficModeStatus :
+          params.put_bool("ChangTrafficModeStat", False)
+          params.put_bool("ChangTrafficModeReq", True)
         self.params.put_nonblocking('LongitudinalPersonality', str(rlx_personality))
     self.personality_timer -= 1
 
