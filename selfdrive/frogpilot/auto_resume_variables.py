1f275b0f1cb04731dc0d3d6ee76d489247e80add
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-05-09 15:13:02 +0300

    Update auto_resume_variables.py

--- selfdrive/frogpilot/auto_resume_variables.py
+++ selfdrive/frogpilot/auto_resume_variables.py
@@ -4,13 +4,13 @@
 send_resume_button = False  
   
 def set_resume_button(value):  
-    global send_resume_button  
-    send_resume_button = value  
+  global send_resume_button  
+  send_resume_button = value  
   
 def get_resume_button():  
-    global send_resume_button  
-    return send_resume_button  
+  global send_resume_button  
+  return send_resume_button  
   
 def reset_resume_button():  
-    global send_resume_button  
-    send_resume_button = False
+  global send_resume_button  
+  send_resume_button = False
