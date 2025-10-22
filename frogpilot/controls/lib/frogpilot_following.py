c73e18a71fad46311da56037980f7df7b0c7578b
nkaoud <56480278+nkaoud@users.noreply.github.com>
2025-10-22 11:59:45 +0300

    Update frogpilot_following.py

--- frogpilot/controls/lib/frogpilot_following.py
+++ frogpilot/controls/lib/frogpilot_following.py
@@ -41,22 +41,21 @@ class FrogPilotFollowing:
       
       # The "steepness" of the curve. > 1.0 = slower start, < 1.0 = faster start
       #curve_factor = 3.0 #2.0
-      cf_follow =  3.0 #2.0
-      cf_jerk_acceleration     =  3.0 #2.0
-      cf_jerk_deceleration     =  3.0 #2.0
-      cf_jerk_danger           =  3.0 #2.0
-      cf_jerk_speed            =  3.0 #2.0
-      cf_jerk_speed_decrease   =  3.0 #2.0
+      cf_follow                =  1.3 #2.0
+      cf_jerk_acceleration     =  0.8 #2.0
+      cf_jerk_deceleration     =  0.6 #2.0
+      cf_jerk_speed            =  0.7 #2.0
+      cf_jerk_speed_decrease   =  1.2 #2.0
+      cf_jerk_danger           =  1.5 #2.0
 
-      # Follow distance in seconds [Traffic Mode default, Relaxed Mode default]
-      dynamic_follow = [0.5, 1.75]
 
-      # Jerk multipliers [Traffic Mode default, Relaxed Mode default]
-      dynamic_jerk_acceleration     = [0.5, 1.0]
-      dynamic_jerk_deceleration     = [0.5, 1.0]
-      dynamic_jerk_danger           = [1.0, 1.0]
-      dynamic_jerk_speed            = [0.5, 1.0]
-      dynamic_jerk_speed_decrease   = [0.5, 1.0]
+      # multipliers [Traffic Mode default, Relaxed Mode default]
+      dynamic_follow                = [0.5, 1.75]    #[0.5, 1.75]
+      dynamic_jerk_acceleration     = [0.5, 1.0]    #[0.5, 1.0]
+      dynamic_jerk_deceleration     = [0.5, 1.0]    #[0.5, 1.0]
+      dynamic_jerk_speed            = [0.5, 1.0]    #[0.5, 1.0]
+      dynamic_jerk_speed_decrease   = [0.5, 1.0]    #[0.5, 1.0]
+      dynamic_jerk_danger           = [0.75, 1.0]    #[1, 1.0]
 
       # ======================================================================
       # ======================= END OF CUSTOM VALUES =========================
