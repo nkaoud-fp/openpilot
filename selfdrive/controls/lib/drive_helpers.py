b8a186138ba4840ed3a54e22293a5ac08e51c7da
nkaoud <56480278+nkaoud@users.noreply.github.com>
2026-01-24 12:52:20 +0300

    Update drive_helpers.py

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -64,8 +64,8 @@ class VCruiseHelper:
         self.v_cruise_cluster_kph = self.v_cruise_kph
         self.update_button_timers(CS, enabled)
       else:
-        self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
-        self.v_cruise_cluster_kph = CS.cruiseState.speedCluster * CV.MS_TO_KPH
+        self.v_cruise_kph = self.v_cruise_kph_last # CS.cruiseState.speed * CV.MS_TO_KPH
+        self.v_cruise_cluster_kph = self.v_cruise_kph_last # CS.cruiseState.speedCluster * CV.MS_TO_KPH
         if CS.cruiseState.speed == 0:
           self.v_cruise_kph = V_CRUISE_UNSET
           self.v_cruise_cluster_kph = V_CRUISE_UNSET
