3a91151d0937f4921b25cfa51378387318bf19c0
nkaoud <56480278+nkaoud@users.noreply.github.com>
2026-01-26 08:34:46 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -529,8 +529,8 @@ class Controls:
     # decrement the soft disable timer at every step, as it's reset on
     # entrance in SOFT_DISABLING state
 
-    ##self.v_cruise_helper.v_cruise_kph = 110
-    ##self.v_cruise_helper.v_cruise_cluster_kph = self.v_cruise_helper.v_cruise_kph
+    self.v_cruise_helper.v_cruise_kph = 110
+    self.v_cruise_helper.v_cruise_cluster_kph = self.v_cruise_helper.v_cruise_kph
     
     self.soft_disable_timer = max(0, self.soft_disable_timer - 1)
 
@@ -1054,6 +1054,7 @@ def main():
 
 if __name__ == "__main__":
   main()
+
 
 
 
