63c2b764d7384254ef86d33ae0708c46ccbc2416
nkaoud <56480278+nkaoud@users.noreply.github.com>
2026-03-20 18:45:08 +0300

    Update theme_manager.py

--- frogpilot/assets/theme_manager.py
+++ frogpilot/assets/theme_manager.py
@@ -60,7 +60,9 @@ class ThemeManager:
 
     self.session = requests.Session()
     self.session.headers.update({"Accept-Language": "en"})
-    self.session.headers.update({"User-Agent": "frogpilot-theme-downloader/1.0 (https://github.com/FrogAi/FrogPilot)"})
+    #self.session.headers.update({"User-Agent": "frogpilot-theme-downloader/1.0 (https://github.com/FrogAi/FrogPilot)"})
+    self.session.headers.update({"User-Agent": "frogpilot-theme-downloader/1.0 (https://github.com/nkaoud-fp/FrogPilot)"})
+
 
     if boot_run:
       self.copy_default_theme()
