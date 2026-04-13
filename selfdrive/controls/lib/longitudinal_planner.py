74c7878716f341d76836418af017f974e95cf65d
nkaoud <56480278+nkaoud@users.noreply.github.com>
2026-04-13 14:08:56 +0300

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -71,6 +71,7 @@ class LongitudinalPlanner:
     self.output_should_stop = False
     ### ADD THIS: Initialize our Dynamic Experimental-mode decel softening V2 
     self.dynamic_model_decel_cap = -1.7 ### m/s^2 cap for model decel in experimental, Limits how hard the model is allowed to brake in experimental mode. More negative = allows stronger braking. Less negative (e.g., -1.2) = softer, longer stops. (from -2.5 to -1.0)
+    self.dynamic_model_decel_recovery_step = 0.01   # lower is slower return to baseline cap higher is faster return to cap (use from 0.01 to 1)
     #EXP_MODEL_DECEL_BLEND = 1.0 ### weight for model decel; remainder comes from MPC (0.30 here), Mix between model and ACC MPC decel. 1.0 = use model fully (sharper). 0.0 = use MPC only (more like chill). Lowering this makes experimental behave more like ACC. (from 0.4 and 0.85 )
 
     self.v_desired_trajectory = np.zeros(CONTROL_N)
@@ -195,7 +196,10 @@ class LongitudinalPlanner:
         self.dynamic_model_decel_cap = max(self.dynamic_model_decel_cap, -3.5)
       else:
         # If the model eases up, reset the cap back to the baseline
-        self.dynamic_model_decel_cap = -1.7   ###  CAP is -1.7
+        #self.dynamic_model_decel_cap = -1.7   ###  CAP is -1.7
+        self.dynamic_model_decel_cap += self.dynamic_model_decel_recovery_step
+        self.dynamic_model_decel_cap = min(self.dynamic_model_decel_cap, -1.7) ######### soft return to cap #############
+
       # 2. Cap the model using the new dynamic cap
       model_a = max(output_a_target_e2e, self.dynamic_model_decel_cap)      
       # 3. Blend the capped model with the MPC
